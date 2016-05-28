ae04a4c4ca9c64619d331268732696f4df48a2b5
KimSeyeong <seyeong@KimSeyeongui-MacBook-Pro.local>
2016-05-28 15:37:04 +0900

    group_member2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528033548) do
+ActiveRecord::Schema.define(version: 20160528055504) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -43,5 +43,12 @@ ActiveRecord::Schema.define(version: 20160528033548) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_groups", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
